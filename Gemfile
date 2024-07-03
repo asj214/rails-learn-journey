af089c3b92b1296094b004da8e8dec69b7f2b8d5
sjahn <asj214@naver.com>
2024-07-03 17:21:11 +0900

    JWT 인증 방식 추가

--- Gemfile
+++ Gemfile
@@ -50,6 +50,7 @@ gem "bootsnap", require: false
 gem 'dotenv-rails', '~> 3.1', '>= 3.1.2'
 
 gem 'devise'
+gem 'jwt'
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -8,7 +8,7 @@ class AuthController < ApplicationController
     if user.save
       render json: user, status: :created
     else
-      render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+      render json: { errors: user.errors.full_messages }, status: :bad_request
     end
   end
 
@@ -17,14 +17,24 @@ class AuthController < ApplicationController
     user = User.find_by_email(user_params[:email])
 
     if user.valid_password?(user_params[:password])
-      render plain: 'Authenticated!'
+      token = JWT.encode({
+        id: user.id,
+        exp: 30.days.from_now.to_i
+      }, Rails.application.credentials.secret_key_base)
+
+      payload = {
+        :token => token,
+        :user => user
+      }
+
+      render json: payload
     else
-      render plain: 'Error!!'
+      render json: {errors: ['Invalid email/password']}, status: :unauthorized
     end
   end
 
   def me
-    render plain: 'auth#me'
+    render json: @current_user
   end
 
 end
